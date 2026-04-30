d1cb98d70c792dbefd2f0d1c2af253c6ce1b8b29
Rupesh <Rupeshkumar6410@gmail.com>
2026-04-30 21:17:12 +0530

    3sum smaller

---
+++ 3sumsmaller.py
@@ -0,0 +1,20 @@
+class Solution:
+    def countTriplets(self, sum, arr):
+        arr.sort()
+        n = len(arr)
+        ans = 0
+
+        for i in range(n - 2):
+            l = i + 1
+            r = n - 1
+
+            while l < r:
+                curr_sum = arr[i] + arr[l] + arr[r]
+
+                if curr_sum < sum:
+                    ans += (r - l)   # key logic
+                    l += 1
+                else:
+                    r -= 1
+
+        return ans
