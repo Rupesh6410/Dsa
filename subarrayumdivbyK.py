37acdcc33f1c95f81a15304c12cfe8dc6f1d51f5
Rupesh <Rupeshkumar6410@gmail.com>
2026-05-11 07:44:10 +0530

    subarraysumdivbyk

---
+++ subarrayumdivbyK.py
@@ -0,0 +1,16 @@
+class Solution:
+    def subarraysDivByK(self, nums: List[int], k: int) -> int:
+        sum=0
+        ans=0
+        res=0
+        f={}
+        f[0]=1
+        for i in range(len(nums)):
+            sum+=nums[i]
+            res=sum%k
+            if res in f:
+                ans+=f[res]
+                f[res]+=1
+            else:
+                f[res]=1
+        return ans
